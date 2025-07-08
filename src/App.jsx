311a15b9ddfe9e0912dd4b3985190b80279df5e2
sp-177 <shub404.x@gmail.com>
2025-07-08 13:18:09 +0530

    "Backend"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+// src/App.jsx
 function App() {
   return (
-    <div className="min-h-screen bg-blue-50 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600 ">
-        Tailwind CSS is working!
-      </h1>
+    <div className="p-6 space-y-4">
+      <h1 className="text-3xl font-bold">🌼 DaisyUI + Tailwind + React</h1>
+
+      <button className="btn btn-primary">Primary Button</button>
+      <button className="btn btn-secondary">Secondary</button>
+
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>This is a simple DaisyUI card.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-success">Okay</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
